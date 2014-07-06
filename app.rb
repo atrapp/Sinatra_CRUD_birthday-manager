916664a3b15754f27f518749e4854c997d2d067f
atrapp <andrea.trapp@gmx.net>
2014-07-06 16:22:42 -0400

    adds the routes

--- app.rb
+++ app.rb
@@ -12,3 +12,171 @@ require_relative 'config.rb'
 get '/' do
   erb :index
 end
+
+# binding.pry
+
+
+# *****   Routes for people *****
+
+# index
+get '/people'
+  @people = Person.all
+  erb :'people/index'  
+end
+
+# new
+get '/people/new'
+  erb :'people/new'
+end
+
+# create
+post '/people'
+  name = params[:name]
+  gender = params[:gender]
+  date_of_birth = params[:date_of_birth]
+  image_url = params[:image_url]
+  country_id = params[:country_id]
+  group_id = params[:group_id]
+
+  new_person = Person.create({name: name, gender: gender, date_of_birth: date_of_birth, image_url: image_url, country_id: country_id, group_id: group_id})
+  redirect "/people/#{new_person.id}"
+end
+
+# edit
+get '/people/:id/edit'
+  @person = Person.find(params[:id])
+  erb :'people/edit'
+end
+
+# update
+put '/people/:id'
+  updated_person = Person.find(params[:id])
+
+  updated_person.name = params[:name]
+  updated_person.gender = params[:gender]
+  updated_person.date_of_birth = params[:date_of_birth]
+  updated_person.image_url = params[:image_url]
+  updated_person.country_id = params[:country_id]
+  updated_person.group_id = params[:group_id]
+
+  updated_person.save
+  redirect "/people/#{updated_person.id}"
+end
+
+# show
+get '/people/:id'
+  @person = Person.find(params[:id])
+  erb :'people/show'
+end
+
+# delete
+delete '/people/:id'
+  Person.delete(params[:id])
+  redirect '/people'
+end
+
+
+# *****   Routes for countries *****
+
+# index
+get '/countries'
+  @countries = Country.all
+  erb :'countries/index'
+end
+
+# new
+get '/countries/new'
+  erb :'countries/new'
+end
+
+# create
+post '/countries'
+  name = params[:name]
+  countrycode = params[:countrycode]
+  description = params[:description]
+  image_url = params[:image_url]
+
+  new_country = Country.create({name: name, countrycode: countrycode, description: description, image_url: image_url})
+  redirect "/countries/#{new_country.id}"
+end
+
+# edit
+get '/countries/:id/edit'
+  @country = Country.find(params[:id])
+  erb :'countries/edit'
+end
+
+# update
+put '/countries/:id'
+  updated_country = Country.find(params[:id])
+
+  updated_country.name = params[:name]
+  updated_country.countrycode = params[:countrycode]
+  updated_country.description = params[:description]
+  updated_country.image_url = params[:image_url]
+
+  updated_country.save
+  redirect "/countries/#{updated_country.id}"
+end
+
+# show
+get '/countries/:id'
+  @country = Country.find(params[:id])
+  erb :'countries/show'
+end
+
+# delete
+delete '/countries/:id'
+  Country.delete(params[:id])
+  redirect "/countries"
+end
+
+
+# *****   Routes for groups *****
+
+# index
+get '/groups'
+  @groups = Group.all
+  erb :'groups/index'
+end
+
+# new
+get '/groups/new'
+  erb :'groups/new'
+end
+
+# create
+post '/groups'
+  name = params[:name]
+
+  new_group = Group.create({name: name})
+  redirect "/groups/#{new_group.id}"
+end
+
+# edit
+get '/groups/:id/edit'
+  @group = Group.find(params[:id])
+  erb :'groups/edit'
+end
+
+# update
+put '/groups/:id'
+  updated_group = Group.find(params[:id])
+
+  updated_group.name = params[:name]
+
+  updated_group.save
+  redirect "/groups/#{updated_group.id}"
+end
+
+# show
+get '/groups/:id'
+  @group = Group.find(params[:id])
+  erb :'groups/show'
+end
+
+# delete
+delete '/groups/:id'
+  Group.delete(params[:id])
+  redirect "/groups"
+end
--- birthday_manager_log.rb
+++ birthday_manager_log.rb
@@ -19,3 +19,15 @@ class Group < ActiveRecord::Base
   has_many :people
 end
 
+# testing
+country = Country.create({name: 'England', countrycode: 'UK', description: 'England is a country that is part of the United Kingdom', image_url: "http://hewgill.com/flags/gb.svg"})
+group = Group.create({name: 'Friends'})
+person = Person.create({name: 'Linda', gender: 'F', date_of_birth: "10/01/1980", image_url: "https://openclipart.org/image/800px/svg_to_png/181239/1375025979.png"})
+
+country.people << person
+group.people << person
+
+# result
+puts "Person name:  " + person.name
+puts "Person country:   " + person.country.name
+puts "Person group: " + person.group.name
